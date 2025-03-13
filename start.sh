0424764dcd86ee71c0cff71d98337312d080fbb2
gyoungsubLee <dltjq2323@gmail.com>
2025-03-13 14:48:03 +0900

    sh: Fix/start.sh
    issue: # [브랜치명]

--- start.sh
+++ start.sh
@@ -3,17 +3,17 @@
 # 현재 스크립트가 위치한 디렉토리로 이동 (어디서 실행해도 정상 동작)
 cd "$(dirname "$0")"
 
-# 현재 실행 중인 컨테이너 종료
-echo "🔨 compose 종료..."
-docker compose down || { echo "❌ compose 종료 실패!"; exit 1; }
+echo "🔨 Gradle Build 시작..."
+./gradlew clean build -x test || { echo "❌ Gradle Build 실패!"; exit 1; }
 
-# start.sh 실행
-echo "🔨 compose 재실행..."
-./start.sh
+echo "🚀 Docker Compose 실행..."
+docker compose up -d --build
 
+echo "📋 Docker compose ps 실행..."
+docker compose ps
 
 # 실행 전  권한 부여 필요
-# chmod +x restart.sh
+# chmod +x start.sh
 
-# 실행 cmd
-# ./restart.sh
+# 실행
+# ./start.sh
